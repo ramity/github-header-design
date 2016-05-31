ea3b31f131838110f6504be7285417a5f8359347
ramity <lcb005@uark.edu>
2016-05-31 11:07:53 -0500

    init commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+//print_r($_SERVER);
+?>
+<!DOCTYPE html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="https://comp.uark.edu/~lcb005/css/index.css">
+	</head>
+	<body>
+		<div id="header">
+			<div id="headerinr">
+				<a href="https://comp.uark.edu/~lcb005/">
+					<div id="headerlogo">Ramity Hub</div>
+				</a>
+				<form id="headersearch" method="post" action="https://comp.uark.edu/~lcb005/search.php">
+					<input id="headersearchinput" type="text" placeholder="Search" autocomplete="off">
+				</form>
+				<div id="headerlinks">
+					<a href="">
+						<div class="headerlinkitem">Home</div>
+					</a>
+					<a href="">
+						<div class="headerlinkitem">Blog</div>
+					</a>
+					<a href="">
+						<div class="headerlinkitem">Support</div>
+					</a>
+					<a href="https://comp.uark.edu/~lcb005/register.php">
+						<div id="headerlinkregister">Register</div>
+					</a>
+					<a href="https://comp.uark.edu/~lcb005/login.php">
+						<div id="headerlinklogin">Login</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
